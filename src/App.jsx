7ca63976b6e565168b329e8d59481ec9426d0da4
Chumki khatun <chumkikhatun370@gmail.com>
2024-02-06 11:33:01 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl text-gray-300'>Quiz App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
